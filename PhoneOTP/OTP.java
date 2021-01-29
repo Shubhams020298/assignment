8dc6518a2dc5aa1b94ced9f6e4f79d7db0921f53
Shubhams020298 <78192581+Shubhams020298@users.noreply.github.com>
2021-01-29 17:55:11 +0530

    Update OTP.java

--- PhoneOTP/OTP.java
+++ PhoneOTP/OTP.java
@@ -23,57 +23,17 @@ public class OTP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          try {
-			// Construct data
-                        //apikey="BrA1F+KgM4Q-TNdL94dgYoy7UJhv7fMEblJH06SlFh";
-			String apiKey = "apikey=" + "BrA1F+KgM4Q-TNdL94dgYoy7UJhv7fMEblJH06SlFh";
-                      // Random rander=new Random();
-                        String message = "&message=" + "Hey Shubham Your OTP is ";
-			//String message = "&message=" + txtmessage.getText();
-			//String sender = "&sender=" + txtSender.getText();
-			String numbers = "&numbers=" + "8070643033";
-                        //String number= textPhone.getText();
-			//otp=rander.nextInt(999999);
-                        //String name= textName.getText();
-                        
-			// Send data
-			//HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
-                       HttpURLConnection conn = (HttpURLConnection) new URL("https://api.txtlocal.com/send/?").openConnection();
-			String data = apiKey + numbers + message;
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-			conn.getOutputStream().write(data.getBytes("UTF-8"));
-			final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			final StringBuffer stringBuffer = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				stringBuffer.append(line);
-                              //  JOptionPane.showMessageDialog(null," message"+line);
-			}
-                        System.out.println(stringBuffer.toString());
-			
-			rd.close();
-			//JOptionPane.showConfirmDialog(null, "OTP Send Succssfully ");
-                                //return stringBuffer.toString();
-		} catch (Exception e) {
-			//System.out.println("Error SMS "+e);
-                        //JOptionPane.showMessageDialog(null,"Error sms"+e);
-                      //  JOptionPane.showMessageDialog(null, "error"+e);
-			//return "Error "+e;
-                     System.out.println("Error SMS "+e);
-		}
-      /*  int otp=0;
+         
         try {
 			// Construct data
 			String apiKey = "apikey=" + "BrA1F+KgM4Q-PI5rF6KWh5cxOSGkXrb3eENnebs2WF";
-                        //Random rander=new Random();
+                        Random rander=new Random();
 			String message = "&message=" + "Hey Shubham Your OTP is ";
-			//String sender = "&sender=" + "TXTLCL";
+			String sender = "&sender=" + "TXTLCL";
 			String numbers = "&numbers=" + "8070643033" ;
-                        //String number= textPhone.getText();
-			//otp=rander.nextInt(999999);
-                      //  String name= "Shubham";
+                        String number= textPhone.getText();
+			otp=rander.nextInt(999999);
+                        String name= "Shubham";
                         
                         
 			// Send data
@@ -100,7 +60,7 @@ public class OTP {
                      //   JOptionPane.showMessageDialog(null,"Error sms"+e);
                       //  JOptionPane.showMessageDialog(null, "error"+e);
 			//return "Error "+e;
-		}   */     
+		}   
     }                         
     }
     
